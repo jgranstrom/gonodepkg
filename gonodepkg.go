2e55b89d08de2aad173d11a72aa521622bc3b5b1
jgranstrom <granstrom.john@gmail.com>
2013-04-01 18:26:20 +0200

    Significant changes to use new Json library for performance and ease of use

--- gonodepkg.go
+++ gonodepkg.go
@@ -23,8 +23,8 @@
 package gonodepkg
 
 import (
-	"bufio"
-	"encoding/json"
+	json "github.com/jgranstrom/go-simplejson"
+	"bufio"	
 	"fmt"
 	"io"
 	"os"
@@ -34,24 +34,8 @@ import (
 const signal_NOSIGNAL = -1
 const signal_TERMINATION = 1
 
-// Defines a raw command object transmitted over gonode - internal type
-type command struct {
-	Id     int
-	Cmd    CommandData
-	Signal int
-}
-
-// Defines a raw response object transmitted over gonode - internal type
-type response struct {
-	Id   int         `json:"id"`   // Make sure response is in lowercase
-	Data CommandData `json:"data"` // Make sure response is in lowercase
-}
-
 // Processor is the provided function that handles each command and provides a result
-type Processor func(cmd CommandData) (resp CommandData)
-
-// CommandData is a wrapping type for representing the JSON objects transmitted
-type CommandData map[string]interface{}
+type Processor func(cmd *json.Json) (resp *json.Json)
 
 // Start the gonode listener which will enter an endless loop while waiting for commands
 // Each command will be delegated to new go-routines and processed by the provided Processor.
@@ -75,14 +59,28 @@ func Start(proc Processor) {
 			continue
 		}
 
-		// Parse JSON into Command struct
-		var c command
-		json.Unmarshal([]byte(s), &c)
+		// Parse JSON into command data
+		cmd, err := json.NewJson([]byte(s))
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		id, err := cmd.Get("id").Int()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		signal, err := cmd.Get("signal").Int()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		cmdData := cmd.Get("cmd")
 
 		// Handle input
-		switch c.Signal {
+		switch signal {
 		case signal_NOSIGNAL:
-			go handle(c, proc) // Handle commands on new go-routine
+			go handle(id, cmdData, proc) // Handle commands on new go-routine
 		case signal_TERMINATION:
 			return // Abort loop on termination
 		}
@@ -90,11 +88,15 @@ func Start(proc Processor) {
 }
 
 // Handle a command by invoking processor and send result on stdout
-func handle(c command, proc Processor) {
+func handle(id int, cmd *json.Json, proc Processor) {
 	// Create a response with the matching ID
-	var r response
-	r.Id = c.Id
-	r.Data = proc(c.Cmd) // Set response data to processor result
-	b, _ := json.Marshal(r)
+	r, dat, err := json.MakeMap()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	dat["id"] = id
+	dat["data"] = proc(cmd) // Set response data to processor result	
+	b, _ := r.Encode()
 	fmt.Println(string(b)) // Send JSON result on stdout
 }
