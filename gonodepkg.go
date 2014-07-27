140c879deb0c3562892feed1ce496ca2b9b44698
Collin Glass <collinglass@gmx.com>
2014-07-27 07:40:30 -0400

    add _ for error

--- gonodepkg.go
+++ gonodepkg.go
@@ -90,7 +90,7 @@ func Start(proc Processor) {
 // Handle a command by invoking processor and send result on stdout
 func handle(id int, cmd *json.Json, proc Processor) {
 	// Create a response with the matching ID
-	r, dat := json.MakeMap()
+	r, dat, _ := json.MakeMap()
 	dat["id"] = id
 	dat["data"] = proc(cmd) // Set response data to processor result	
 	b, _ := r.Encode()
